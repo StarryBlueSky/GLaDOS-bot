35a11c143baf1b70426c6e359a14224ea4bb87d3
Nep <spica@starry.blue>
2018-03-30 15:14:34 +0900

    Fix: Windowsでyoutube-dlが使用できない問題を修正

--- src/main/kotlin/jp/nephy/glados/component/api/youtubedl/YouTubeDL.kt
+++ src/main/kotlin/jp/nephy/glados/component/api/youtubedl/YouTubeDL.kt
@@ -4,47 +4,31 @@ import jp.nephy.glados.component.api.youtubedl.model.YouTubeDLInfo
 import jp.nephy.jsonkt.JsonKt
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.nio.file.Path
+import java.util.concurrent.TimeUnit
 
 
 class YouTubeDL(private val videoUrl: String) {
-    private var cachedVideoStream: InputStream? = null
     private var cachedVideoInfo: YouTubeDLInfo? = null
 
-    private fun getResultStream(vararg args: String): InputStream {
-        val processBuilder = ProcessBuilder("youtube-dl", *args, videoUrl)
-        val process = processBuilder.start()
-        process.waitFor()
-        return process.inputStream
+    private fun getResultStream(vararg args: String): InputStream? {
+        val process = ProcessBuilder("youtube-dl", *args, videoUrl).start()
+        process.waitFor(1, TimeUnit.SECONDS)
+        return process.inputStream ?: null
     }
-    private fun getResultAsString(vararg args: String): String {
-        return InputStreamReader(getResultStream(*args)).use { it.readText() }.trim()
-    }
-
-    fun download(path: Path): Boolean {
-        return try {
-            getResultStream("-o", path.toString())
-            true
-        } catch (e: Exception) {
-            println(e.localizedMessage)
-            false
-        }
+    private fun getResultAsString(vararg args: String): String? {
+        val stream = getResultStream(*args) ?: return null
+        return InputStreamReader(stream).use { it.readText() }.trim()
     }
 
-    val videoStream: InputStream?
-        get() = cachedVideoStream ?: try {
-            getResultStream("-o", "-").also {
-                cachedVideoStream = it
-            }
-        } catch (e: Exception) {
-            null
-        }
     val info: YouTubeDLInfo?
-        get() = cachedVideoInfo ?: try {
-            JsonKt.parse<YouTubeDLInfo>(getResultAsString("-j")).also {
-                cachedVideoInfo = it
+        get() {
+            return cachedVideoInfo ?: try {
+                val result = getResultAsString("-j") ?: return null
+                JsonKt.parse<YouTubeDLInfo>(result).also {
+                    cachedVideoInfo = it
+                }
+            } catch (e: Exception) {
+                null
             }
-        } catch (e: Exception) {
-            null
         }
 }
