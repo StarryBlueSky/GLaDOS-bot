4af4491b611467f3fc644ba5a1dca8bea320193b
Nep <spica@starry.blue>
2018-03-30 15:18:15 +0900

    Revert: 無音バグの原因ではなかったのでJDA-NASを再び追加

--- src/main/kotlin/jp/nephy/glados/GLaDOS.kt
+++ src/main/kotlin/jp/nephy/glados/GLaDOS.kt
@@ -2,6 +2,7 @@ package jp.nephy.glados
 
 import com.jagrosh.jdautilities.command.CommandClientBuilder
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter
+import com.sedmelluq.discord.lavaplayer.jdaudp.NativeAudioSendFactory
 import jp.nephy.glados.component.MessageCacheManager
 import jp.nephy.glados.component.api.ApiClient
 import jp.nephy.glados.component.audio.music.PlayerManager
@@ -69,7 +70,7 @@ class GLaDOS(val config: GLaDOSConfig, val parameter: GLaDOSParameter, val secre
 
     val jda = JDABuilder(AccountType.BOT)
             .setToken(config.token)
-            //.setAudioSendFactory(NativeAudioSendFactory())
+            .setAudioSendFactory(NativeAudioSendFactory())
             .addEventListener(eventWaiter)
             .addEventListener(client)
             .apply {
