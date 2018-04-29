19f6f56f1a3f935ca333a6a8c1c27074041507a1
Nep <spica@starry.blue>
2018-04-29 14:51:44 +0900

    Add: TwitterのSecretConfigを追加

--- src/main/kotlin/jp/nephy/glados/component/api/ApiClient.kt
+++ src/main/kotlin/jp/nephy/glados/component/api/ApiClient.kt
@@ -5,14 +5,10 @@ import jp.nephy.glados.GLaDOS
 import jp.nephy.glados.component.api.niconico.NiconicoClient
 import jp.nephy.glados.component.api.soundcloud.SoundCloudClient
 import jp.nephy.glados.component.api.youtube.YouTubeClient
-import jp.nephy.penicillin.PenicillinClient
 
 class ApiClient(bot: GLaDOS) {
     val soundCloud = SoundCloudClient(bot.secret.soundCloudClientId)
-    val twitter = PenicillinClient.build {
-        application("", "")
-        token("", "")
-    }
+    val twitter = bot.secret.twiter
     val niconico = NiconicoClient()
     val youtube = YouTubeClient(bot.secret.googleApiKey)
     val steam = SteamWebApiClient.SteamWebApiClientBuilder(bot.secret.steamApiKey).build()!!
--- src/main/kotlin/jp/nephy/glados/component/config/SecretConfig.kt
+++ src/main/kotlin/jp/nephy/glados/component/config/SecretConfig.kt
@@ -1,10 +1,13 @@
 package jp.nephy.glados.component.config
 
+import jp.nephy.penicillin.PenicillinClient
+
 data class SecretConfig(
         val slackIncomingUrl: String,
         val niconicoLoginEmail: String,
         val niconicoLoginPassword: String,
         val soundCloudClientId: String,
         val googleApiKey: String,
-        val steamApiKey: String
+        val steamApiKey: String,
+        val twiter: PenicillinClient
 )
