cd5ebe5082d4f0f5f539df51fc75a5bdd53d5359
Nep <spica@starry.blue>
2018-05-19 21:51:08 +0900

    Fix: 陰キャロールが適切に管理されない問題を修正

--- src/main/kotlin/jp/nephy/glados/feature/listener/kaigen/Inkya.kt
+++ src/main/kotlin/jp/nephy/glados/feature/listener/kaigen/Inkya.kt
@@ -1,12 +1,12 @@
 package jp.nephy.glados.feature.listener.kaigen
 
-import jp.nephy.glados.GLaDOS
-import jp.nephy.glados.component.helper.hasRole
+import jp.nephy.glados.component.helper.addRole
+import jp.nephy.glados.component.helper.removeRole
 import jp.nephy.glados.feature.ListenerFeature
 import jp.nephy.glados.logger
-import net.dv8tion.jda.core.JDA
 import net.dv8tion.jda.core.entities.Guild
 import net.dv8tion.jda.core.entities.Member
+import net.dv8tion.jda.core.entities.Role
 import net.dv8tion.jda.core.events.ReadyEvent
 import net.dv8tion.jda.core.events.guild.member.GuildMemberRoleRemoveEvent
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent
@@ -18,22 +18,28 @@ import kotlin.concurrent.thread
 
 
 class Inkya: ListenerFeature() {
+    private val enabledGuilds = mutableMapOf<Guild, Role>()
+
     override fun onReady(event: ReadyEvent) {
+        event.jda.guilds.forEach {
+            val config = bot.config.getGuildConfig(it)
+            if (config.role.inkya == null) {
+                return@forEach
+            }
+
+            enabledGuilds[it] = it.getRoleById(config.role.inkya) ?: return@forEach
+        }
+
         // 10秒おきに陰キャロールが適切かを調べるスレッド
         thread {
             while (true) {
                 try {
-                    event.jda.guilds.forEach { guild ->
-                        val config = GLaDOS.instance.config.getGuildConfig(guild)
-                        if (config.role.inkya == null) {
-                            return@forEach
-                        }
-
+                    enabledGuilds.keys.forEach { guild ->
                         guild.members.forEach {
-                            if (! it.voiceState.inVoiceChannel() || it.voiceState.channel == guild.afkChannel || ! it.voiceState.isSelfMuted) {
-                                removeRole(guild, it, event.jda)
+                            if (it.voiceState.inVoiceChannel() && it.voiceState.channel != guild.afkChannel && it.voiceState.isSelfMuted) {
+                                it.addInkyaRole()
                             } else {
-                                addRole(guild, it, event.jda)
+                                it.removeInkyaRole()
                             }
                         }
                     }
@@ -46,26 +52,12 @@ class Inkya: ListenerFeature() {
         }
     }
 
-    private fun addRole(guild: Guild, member: Member, jda: JDA) {
-        val config = bot.config.getGuildConfig(guild)
-        if (config.role.inkya == null) {
-            return
-        }
-
-        if (! member.hasRole(config.role.inkya)) {
-            guild.controller.addSingleRoleToMember(member, jda.getRoleById(config.role.inkya)).queue()
-        }
+    private fun Member.addInkyaRole() {
+        addRole(enabledGuilds[guild] ?: return)
     }
 
-    private fun removeRole(guild: Guild, member: Member, jda: JDA) {
-        val config = bot.config.getGuildConfig(guild)
-        if (config.role.inkya == null) {
-            return
-        }
-
-        if (member.hasRole(config.role.inkya)) {
-            guild.controller.removeSingleRoleFromMember(member, jda.getRoleById(config.role.inkya)).queue()
-        }
+    private fun Member.removeInkyaRole() {
+        removeRole(enabledGuilds[guild] ?: return)
     }
 
     // 条件を満たしていないのにロールが外れた場合のチェック
@@ -77,40 +69,40 @@ class Inkya: ListenerFeature() {
 
         event.roles.filter { it.idLong == config.role.inkya }.forEach {
             if (event.member.voiceState.inVoiceChannel() && event.member.voiceState.channel != event.guild.afkChannel && event.member.voiceState.isSelfMuted) {
-                addRole(event.guild, event.member, event.jda)
+                event.member.addInkyaRole()
             }
         }
     }
 
     // VCから切断した場合
     override fun onGuildVoiceLeave(event: GuildVoiceLeaveEvent) {
-        removeRole(event.guild, event.member, event.jda)
+        event.member.removeInkyaRole()
     }
 
     override fun onGuildVoiceMove(event: GuildVoiceMoveEvent) {
-        // AFKチャンネルに移動した場合
         if (event.channelJoined == event.guild.afkChannel) {
-            removeRole(event.guild, event.member, event.jda)
+            // AFKチャンネルに移動した場合
+            event.member.removeInkyaRole()
         } else if (event.channelLeft == event.guild.afkChannel && event.member.voiceState.isSelfMuted) {
             // マイクミュート状態でAFKチャンネルから移動した場合
-            addRole(event.guild, event.member, event.jda)
+            event.member.addInkyaRole()
         }
     }
 
     override fun onGuildVoiceSelfMute(event: GuildVoiceSelfMuteEvent) {
-        // マイクミュートを解除した場合
         if (! event.isSelfMuted) {
-            removeRole(event.guild, event.member, event.jda)
+            // マイクミュートを解除した場合
+            event.member.removeInkyaRole()
         } else if (event.isSelfMuted && event.member.voiceState.channel != event.guild.afkChannel) {
             // マイクミュートした場合 (AFKチャンネル以外)
-            addRole(event.guild, event.member, event.jda)
+            event.member.addInkyaRole()
         }
     }
 
-    // マイクミュート状態でVCに参加した場合 (AFKチャンネル以外)
     override fun onGuildVoiceJoin(event: GuildVoiceJoinEvent) {
+        // マイクミュート状態でVCに参加した場合 (AFKチャンネル以外)
         if (event.member.voiceState.isSelfMuted && event.member.voiceState.channel != event.guild.afkChannel) {
-            addRole(event.guild, event.member, event.jda)
+            event.member.addInkyaRole()
         }
     }
 }
