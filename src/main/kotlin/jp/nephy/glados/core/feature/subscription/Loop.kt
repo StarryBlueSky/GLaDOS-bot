7c253eb1ebaa1599b79fc23f5796b2ff5526f761
Nep <spica@starry.blue>
2018-09-23 16:50:16 +0900

    Update deps

---
+++ src/main/kotlin/jp/nephy/glados/core/feature/subscription/Loop.kt
@@ -0,0 +1,57 @@
+package jp.nephy.glados.core.feature.subscription
+
+import jp.nephy.glados.core.Logger
+import jp.nephy.glados.core.feature.BotFeature
+import kotlinx.coroutines.experimental.CancellationException
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import net.dv8tion.jda.core.events.ReadyEvent
+import net.dv8tion.jda.core.hooks.ListenerAdapter
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.TimeUnit
+import kotlin.reflect.KFunction
+
+private val logger = Logger("GLaDOS.Loop")
+
+data class LoopSubscription(
+        override val annotation: Loop,
+        override val instance: BotFeature,
+        override val function: KFunction<*>
+): Subscription<Loop>
+
+annotation class Loop(
+        val interval: Long,
+        val unit: TimeUnit,
+        val priority: Priority = Priority.Normal
+)
+
+class LoopSubscriptionClient: SubscriptionClient<Loop>, ListenerAdapter() {
+    override val subscriptions = CopyOnWriteArrayList<Subscription<Loop>>()
+
+    override fun onReady() {
+        subscriptions.sortBy { it.annotation.priority }
+    }
+
+    override fun onReady(event: ReadyEvent) {
+        for (it in subscriptions) {
+            launch {
+                while (isActive) {
+                    try {
+                        it.execute()
+                        logger.trace { "${it.instance.javaClass.simpleName}#${it.function.name} が実行されました." }
+                    } catch (e: CancellationException) {
+                        break
+                    } catch (e: Exception) {
+                        logger.error(e) { "[${it.instance.javaClass.simpleName}#${it.function.name}] 実行中に例外が発生しました." }
+                    }
+
+                    try {
+                        delay(it.annotation.interval, it.annotation.unit)
+                    } catch (e: CancellationException) {
+                        break
+                    }
+                }
+            }
+        }
+    }
+}
