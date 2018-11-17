842408b27aff2159da8faf2ce8a7543aac0f8012
Nep <spica@starry.blue>
2018-11-17 23:30:42 +0900

    Add longFragment

--- src/main/kotlin/jp/nephy/glados/core/extensions/web/Request.kt
+++ src/main/kotlin/jp/nephy/glados/core/extensions/web/Request.kt
@@ -6,37 +6,37 @@ import kotlin.reflect.KProperty
 
 fun Plugin.Web.AccessEvent.intQuery(key: String? = null, default: () -> Int) = object: ReadOnlyProperty<Any?, Int> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
-        return this@intQuery.call.parameters[key ?: property.name]?.toIntOrNull() ?: default()
+        return this@intQuery.call.request.queryParameters[key ?: property.name]?.toIntOrNull() ?: default()
     }
 }
 
 fun Plugin.Web.AccessEvent.intQuery(key: String? = null) = object: ReadOnlyProperty<Any?, Int?> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Int? {
-        return this@intQuery.call.parameters[key ?: property.name]?.toIntOrNull()
+        return this@intQuery.call.request.queryParameters[key ?: property.name]?.toIntOrNull()
     }
 }
 
 fun Plugin.Web.AccessEvent.longQuery(key: String? = null, default: () -> Long) = object: ReadOnlyProperty<Any?, Long> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Long {
-        return this@longQuery.call.parameters[key ?: property.name]?.toLongOrNull() ?: default()
+        return this@longQuery.call.request.queryParameters[key ?: property.name]?.toLongOrNull() ?: default()
     }
 }
 
 fun Plugin.Web.AccessEvent.longQuery(key: String? = null) = object: ReadOnlyProperty<Any?, Long?> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Long? {
-        return this@longQuery.call.parameters[key ?: property.name]?.toLongOrNull()
+        return this@longQuery.call.request.queryParameters[key ?: property.name]?.toLongOrNull()
     }
 }
 
 fun Plugin.Web.AccessEvent.query(key: String? = null, default: () -> String) = object: ReadOnlyProperty<Any?, String> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return this@query.call.parameters[key ?: property.name] ?: default()
+        return this@query.call.request.queryParameters[key ?: property.name] ?: default()
     }
 }
 
 fun Plugin.Web.AccessEvent.query(key: String? = null) = object: ReadOnlyProperty<Any?, String?> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): String? {
-        return this@query.call.parameters[key ?: property.name]
+        return this@query.call.request.queryParameters[key ?: property.name]
     }
 }
 
@@ -52,14 +52,27 @@ fun Plugin.Web.AccessEvent.fragment(key: String? = null, default: () -> String) 
     }
 }
 
-fun Plugin.Web.AccessEvent.intVariable(key: String? = null) = object: ReadOnlyProperty<Any?, Int?> {
+fun Plugin.Web.AccessEvent.intFragment(key: String? = null) = object: ReadOnlyProperty<Any?, Int?> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Int? {
         return fragments[key ?: property.name]?.toIntOrNull()
     }
 }
 
-fun Plugin.Web.AccessEvent.intVariable(key: String? = null, default: () -> Int) = object: ReadOnlyProperty<Any?, Int> {
+fun Plugin.Web.AccessEvent.intFragment(key: String? = null, default: () -> Int) = object: ReadOnlyProperty<Any?, Int> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
         return fragments[key ?: property.name]?.toIntOrNull() ?: default()
     }
 }
+
+fun Plugin.Web.AccessEvent.longFragment(key: String? = null) = object: ReadOnlyProperty<Any?, Long?> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Long? {
+        return fragments[key ?: property.name]?.toLongOrNull()
+    }
+}
+
+fun Plugin.Web.AccessEvent.longFragment(key: String? = null, default: () -> Long) = object: ReadOnlyProperty<Any?, Long> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Long {
+        return fragments[key ?: property.name]?.toLongOrNull() ?: default()
+    }
+}
+
