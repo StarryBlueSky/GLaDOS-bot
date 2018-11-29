75212f585d40d74487429302dc5f878d9f7f430e
Nep <spica@starry.blue>
2018-11-29 16:13:43 +0900

    Complete fragment, query

--- src/main/kotlin/jp/nephy/glados/core/plugins/extensions/web/Request.kt
+++ src/main/kotlin/jp/nephy/glados/core/plugins/extensions/web/Request.kt
@@ -1,9 +1,13 @@
+@file:Suppress("UNUSED")
+
 package jp.nephy.glados.core.plugins.extensions.web
 
 import jp.nephy.glados.core.plugins.Plugin
 import kotlinx.serialization.json.toBooleanStrictOrNull
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
+
+/* Query Delegates */
 
 fun Plugin.Web.AccessEvent.booleanQuery(key: String? = null, default: () -> Boolean) = object: ReadOnlyProperty<Any?, Boolean> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Boolean {
@@ -41,6 +45,30 @@ fun Plugin.Web.AccessEvent.longQuery(key: String? = null) = object: ReadOnlyProp
     }
 }
 
+fun Plugin.Web.AccessEvent.floatQuery(key: String? = null, default: () -> Float) = object: ReadOnlyProperty<Any?, Float> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Float {
+        return this@floatQuery.call.request.queryParameters[key ?: property.name]?.toFloatOrNull() ?: default()
+    }
+}
+
+fun Plugin.Web.AccessEvent.floatQuery(key: String? = null) = object: ReadOnlyProperty<Any?, Float?> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Float? {
+        return this@floatQuery.call.request.queryParameters[key ?: property.name]?.toFloatOrNull()
+    }
+}
+
+fun Plugin.Web.AccessEvent.doubleQuery(key: String? = null, default: () -> Double) = object: ReadOnlyProperty<Any?, Double> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Double {
+        return this@doubleQuery.call.request.queryParameters[key ?: property.name]?.toDoubleOrNull() ?: default()
+    }
+}
+
+fun Plugin.Web.AccessEvent.doubleQuery(key: String? = null) = object: ReadOnlyProperty<Any?, Double?> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Double? {
+        return this@doubleQuery.call.request.queryParameters[key ?: property.name]?.toDoubleOrNull()
+    }
+}
+
 fun Plugin.Web.AccessEvent.query(key: String? = null, default: () -> String) = object: ReadOnlyProperty<Any?, String> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): String {
         return this@query.call.request.queryParameters[key ?: property.name] ?: default()
@@ -53,15 +81,17 @@ fun Plugin.Web.AccessEvent.query(key: String? = null) = object: ReadOnlyProperty
     }
 }
 
-fun Plugin.Web.AccessEvent.fragment(key: String? = null) = object: ReadOnlyProperty<Any?, String?> {
-    override fun getValue(thisRef: Any?, property: KProperty<*>): String? {
-        return fragments[key ?: property.name]
+/* Fragment Delegates */
+
+fun Plugin.Web.AccessEvent.booleanFragment(key: String? = null) = object: ReadOnlyProperty<Any?, Boolean?> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Boolean? {
+        return fragments[key ?: property.name]?.toBooleanStrictOrNull()
     }
 }
 
-fun Plugin.Web.AccessEvent.fragment(key: String? = null, default: () -> String) = object: ReadOnlyProperty<Any?, String> {
-    override fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return fragments[key ?: property.name] ?: default()
+fun Plugin.Web.AccessEvent.booleanFragment(key: String? = null, default: () -> Boolean) = object: ReadOnlyProperty<Any?, Boolean> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Boolean {
+        return fragments[key ?: property.name]?.toBooleanStrictOrNull() ?: default()
     }
 }
 
@@ -89,3 +119,38 @@ fun Plugin.Web.AccessEvent.longFragment(key: String? = null, default: () -> Long
     }
 }
 
+fun Plugin.Web.AccessEvent.floatFragment(key: String? = null) = object: ReadOnlyProperty<Any?, Float?> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Float? {
+        return fragments[key ?: property.name]?.toFloatOrNull()
+    }
+}
+
+fun Plugin.Web.AccessEvent.floatFragment(key: String? = null, default: () -> Float) = object: ReadOnlyProperty<Any?, Float> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Float {
+        return fragments[key ?: property.name]?.toFloatOrNull() ?: default()
+    }
+}
+
+fun Plugin.Web.AccessEvent.doubleFragment(key: String? = null) = object: ReadOnlyProperty<Any?, Double?> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Double? {
+        return fragments[key ?: property.name]?.toDoubleOrNull()
+    }
+}
+
+fun Plugin.Web.AccessEvent.doubleFragment(key: String? = null, default: () -> Double) = object: ReadOnlyProperty<Any?, Double> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Double {
+        return fragments[key ?: property.name]?.toDoubleOrNull() ?: default()
+    }
+}
+
+fun Plugin.Web.AccessEvent.fragment(key: String? = null) = object: ReadOnlyProperty<Any?, String?> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): String? {
+        return fragments[key ?: property.name]
+    }
+}
+
+fun Plugin.Web.AccessEvent.fragment(key: String? = null, default: () -> String) = object: ReadOnlyProperty<Any?, String> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return fragments[key ?: property.name] ?: default()
+    }
+}
