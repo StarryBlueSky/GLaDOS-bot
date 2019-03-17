16e09004ad72c22898c69e58acedd8c85134a8bb
Nep <spica@starry.blue>
2019-03-18 03:02:08 +0900

    [glados-client-discord] Update JDA

--- glados-client/glados-client-discord/build.gradle.kts
+++ glados-client/glados-client-discord/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 dependencies {
-    api("net.dv8tion:JDA:4.ALPHA.0_62")
+    api("net.dv8tion:JDA:4.ALPHA.0_63")
     api("com.sedmelluq:lavaplayer:1.3.11")
-    implementation("com.sedmelluq:jda-nas:1.0.6")
+    // implementation("com.sedmelluq:jda-nas:1.0.6")
 }
 
 tasks.withType<KotlinCompile> {
