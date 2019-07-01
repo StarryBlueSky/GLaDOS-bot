163856b1458b92aa3a3bb5532e4608e44366160a
Nep <spica@starry.blue>
2019-07-01 14:06:21 +0900

    [glados-client] Upgrade ktor-client-apache to 1.2.2

--- glados-client/build.gradle.kts
+++ glados-client/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api(project(":glados-api"))
 
     implementation("io.github.microutils:kotlin-logging:1.6.26")
-    implementation("io.ktor:ktor-client-apache:1.2.0")
+    implementation("io.ktor:ktor-client-apache:1.2.2")
 }
 
 subprojects {
