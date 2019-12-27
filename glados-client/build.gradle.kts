210e94ff26bbf953dbe3a2463cecdaaa114463f2
Nep <spica@starry.blue>
2019-12-27 18:26:32 +0900

    [glados-client] Change KotlinLogging to api

--- glados-client/build.gradle.kts
+++ glados-client/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     api(project(":glados-api"))
     api(kotlin("reflect"))
 
-    implementation("io.github.microutils:kotlin-logging:1.7.8")
+    api("io.github.microutils:kotlin-logging:1.7.8")
     api("io.ktor:ktor-client-apache:1.3.0-rc")
 }
 
